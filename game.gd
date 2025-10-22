b1ff45620aaf361529b3188fbb7fff3e232429cb
Gary Storey <garystoreydev@gmail.com>
2025-10-22 14:55:37 -0400

    Refine scenes to follow Godot 4 best practices

--- game.gd
+++ game.gd
@@ -11,80 +11,94 @@ extends Node2D
 
 @onready var player: Area2D = %Player
 
-@export var number_asteroids = 0
-@export var max_scale = 0.2
-@export var points_per_sec = 50
+@export var max_scale: float = 0.2
+@export var points_per_sec: int = 50
+@export var max_asteroids: int = 5
 
-@export var MAX_ASTEROIDS = 5
-@export var MAX_MASS = 250
-@export var MIN_MASS = 50
+var number_asteroids: int = 0
 
-var rng = RandomNumberGenerator.new()
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
-var asteroid_scene = preload("res://asteroid.tscn")
-const level_sfx = preload("uid://c25wdaajvcfhs")
-
+const ASTEROID_SCENE: PackedScene = preload("res://asteroid.tscn")
+const LEVEL_SFX: AudioStream = preload("uid://c25wdaajvcfhs")
 
 func _ready() -> void:
-	score_timer.timeout.connect(_on_score_timer_timeout)
-	round_timer.timeout.connect(_on_round_timer_timeout)
-	player.connect("dead", _on_dead)
-	round_text.text = str(0)
-	score_text.text = str(0)
-	_on_round_timer_timeout()
+    rng.randomize()
 
-func _on_dead():
-	round_timer.stop()
-	score_timer.stop()
-	for asteroid in get_tree().get_nodes_in_group("Asteroids"):
-		asteroid.queue_free()
-	get_tree().change_scene_to_file("res://start.tscn")
-	queue_free()
+    score_timer.timeout.connect(_on_score_timer_timeout)
+    round_timer.timeout.connect(_on_round_timer_timeout)
+    round_display_timer.timeout.connect(_on_display_timeout)
+    player.dead.connect(_on_dead)
+
+    sfx.stream = LEVEL_SFX
+    round_text.text = str(0)
+    score_text.text = str(0)
+
+    _on_round_timer_timeout()
+
+func _on_dead() -> void:
+    round_timer.stop()
+    score_timer.stop()
+
+    for asteroid in get_tree().get_nodes_in_group("Asteroids"):
+        asteroid.queue_free()
+
+    get_tree().change_scene_to_file("res://start.tscn")
+    queue_free()
 
 func _physics_process(_delta: float) -> void:
-	var should_spawn_asteroid = rng.randi_range(0, 100)
-	if should_spawn_asteroid >=80 and number_asteroids< MAX_ASTEROIDS:
-		number_asteroids +=1
-		create_asteroid()
-	
-func create_asteroid():
-		var new_asteroid = asteroid_scene.instantiate();
-		var x = randf_range(0,1100)
-		var new_scale = randf_range(0.05, max_scale)
-		
-		new_asteroid.position = Vector2(x,-250)
-		new_asteroid.get_child(0).scale = Vector2(new_scale,new_scale)
-		new_asteroid.scale =  Vector2(new_scale,new_scale)
-		new_asteroid.mass = 200 * new_scale
-		new_asteroid.gravity_scale = randf_range(0.1,1.3) * new_scale
-		new_asteroid.linear_velocity.x = randf_range(-180, 180)
-		new_asteroid.add_to_group("Asteroids")
-		add_child(new_asteroid)
-		new_asteroid.asteroid_removed.connect(_on_child_asteroid_removed)
-	
-func _on_child_asteroid_removed():
-	number_asteroids -=1
+    var should_spawn_asteroid := rng.randi_range(0, 100)
+    if should_spawn_asteroid >= 80 and number_asteroids < max_asteroids:
+        create_asteroid()
 
-func _on_score_timer_timeout():
-	var score = int(score_text.text)
-	score += randi_range(1,20)
-	score_text.text = str(score)
-	
-func _on_round_timer_timeout():
-	MAX_ASTEROIDS += 5
-	points_per_sec += 50
-	max_scale += 0.05
-	var level = int(round_text.text)
-	sfx.stream = level_sfx
-	sfx.play()
-	level += 1
-	round_text.text = str(level)
-	round_display_timer.start()
-	round_display_timer.timeout.connect(_on_display_timeout)
-	round_display.visible = true
-	round_display.text = "Level "+str(level)
-	
+func create_asteroid() -> void:
+    var new_asteroid: RigidBody2D = ASTEROID_SCENE.instantiate()
+    var spawn_x := rng.randf_range(0.0, 1100.0)
+    var new_scale := rng.randf_range(0.05, max_scale)
 
-func _on_display_timeout():
-	round_display.visible = false
+    new_asteroid.position = Vector2(spawn_x, -250.0)
+
+    var sprite := new_asteroid.get_node("Sprite2D") as Sprite2D
+    if sprite:
+        sprite.scale = Vector2(new_scale, new_scale)
+
+    var collision_shape := new_asteroid.get_node("CollisionShape2D") as CollisionShape2D
+    if collision_shape and collision_shape.shape is CircleShape2D:
+        var shape := (collision_shape.shape as CircleShape2D).duplicate() as CircleShape2D
+        shape.radius *= new_scale
+        collision_shape.shape = shape
+    new_asteroid.mass = 200.0 * new_scale
+    new_asteroid.gravity_scale = rng.randf_range(0.1, 1.3) * new_scale
+    new_asteroid.linear_velocity.x = rng.randf_range(-180.0, 180.0)
+    new_asteroid.add_to_group("Asteroids")
+    new_asteroid.asteroid_removed.connect(_on_child_asteroid_removed)
+
+    add_child(new_asteroid)
+    number_asteroids += 1
+
+func _on_child_asteroid_removed() -> void:
+    number_asteroids = maxi(number_asteroids - 1, 0)
+
+func _on_score_timer_timeout() -> void:
+    var score := int(score_text.text)
+    score += rng.randi_range(1, 20)
+    score_text.text = str(score)
+
+func _on_round_timer_timeout() -> void:
+    max_asteroids += 5
+    points_per_sec += 50
+    max_scale += 0.05
+
+    var level := int(round_text.text)
+    sfx.play()
+
+    level += 1
+    round_text.text = str(level)
+
+    round_display_timer.start()
+    round_display.visible = true
+    round_display.text = "Level " + str(level)
+
+func _on_display_timeout() -> void:
+    round_display.visible = false
 	
--- start.gd
+++ start.gd
@@ -1,13 +1,15 @@
 extends Control
 
-@onready var start = %Button
-var game = preload("uid://b3vu7mn112co7").instantiate()
+@onready var start_button: Button = %Button
 
-func _ready():
-	start.connect("button_up", _start_game)
-	
-func _start_game():
-	get_tree().root.add_child(game)
-	queue_free()
+const GAME_SCENE: PackedScene = preload("uid://b3vu7mn112co7")
+
+func _ready() -> void:
+    start_button.button_up.connect(_start_game)
+
+func _start_game() -> void:
+    var game: Node = GAME_SCENE.instantiate()
+    get_tree().root.add_child(game)
+    queue_free()
 
 	
