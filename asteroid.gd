54f5a1d6f41bad81cdd79402e44f3535550bbcc0
Gary Storey <garystorey655@gmail.com>
2025-10-22 15:17:49 -0400

    add base animation component

--- asteroid.gd
+++ asteroid.gd
@@ -5,7 +5,7 @@ signal asteroid_removed
 @onready var sprite: Node2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
-    sprite.rotate(0.5 * delta)
-    if global_position.y >= 600.0:
-        asteroid_removed.emit()
-        queue_free()
+	sprite.rotate(0.5 * delta)
+	if global_position.y >= 600.0:
+		asteroid_removed.emit()
+		queue_free()
--- start.gd
+++ start.gd
@@ -5,11 +5,11 @@ extends Control
 const GAME_SCENE: PackedScene = preload("uid://b3vu7mn112co7")
 
 func _ready() -> void:
-    start_button.button_up.connect(_start_game)
+	start_button.button_up.connect(_start_game)
 
 func _start_game() -> void:
-    var game: Node = GAME_SCENE.instantiate()
-    get_tree().root.add_child(game)
-    queue_free()
+	var game: Node = GAME_SCENE.instantiate()
+	get_tree().root.add_child(game)
+	queue_free()
 
 	
